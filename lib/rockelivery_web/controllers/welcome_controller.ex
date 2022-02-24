8d9946db1d79ea0283416b6920224a3849511186
alexandra <alexandrabsouz@gmail.com>
2022-02-24 09:18:27 -0300

    feat: creating our first route

---
+++ lib/rockelivery_web/controllers/welcome_controller.ex
@@ -0,0 +1,10 @@
+defmodule RockeliveryWeb.WelcomeController do
+    use RockeliveryWeb, :controller
+    
+
+    def index(conn, params) do
+        conn
+        |> put_status(:ok)
+        |> text("Hello world :D")
+    end
+end
