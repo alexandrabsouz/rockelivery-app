781f1b92aaab4a33574784bb5ad9c0c284da28c6
alexandra <alexandrabsouz@gmail.com>
2022-03-13 09:17:47 -0300

    feat: multiple error status

--- lib/rockelivery/users/create.ex
+++ lib/rockelivery/users/create.ex
@@ -5,5 +5,12 @@ defmodule Rockelivery.Users.Create do
         params
         |> User.changeset()
         |> Repo.insert()
+        |> handle_result()
+    end
+
+    defp handle_result({:ok, %User{}} = result), do: result
+        
+    defp handle_result({:error, result}) do
+        {:error, %{status: :bad_request, result: result}}
     end
 end
