6d6368d8035698f8e6383ad9c283f720f5b086e5
alexandra <alexandrabsouz@gmail.com>
2022-03-22 11:36:30 -0300

    test: create user

---
+++ test/rockelivery/users/create_test.exs
@@ -0,0 +1,24 @@
+defmodule Rockelivery.Users.CreateTest do
+    use Rockelivery.DataCase, async: True
+    import Rockelivery.Factory
+    alias Ecto.Changeset
+    alias Rockelivery.{
+        User,
+        Error
+    }    
+    alias Rockelivery.Users.Create
+
+
+    describe "call/1" do
+        test "when all params are valid, returns an user" do
+            params = build(:user_params)
+            assert {:ok, result} = Create.call(params)
+        end
+
+        test "when params are a invalid changeseat, returns an error" do
+            params = build(:user_params, %{age: 15})
+            assert {:error, %Error{status: :bad_request, result: result}} = Create.call(params)
+            assert errors_on(result) == %{age: ["must be greater than or equal to 18"]}
+        end
+    end
+end
