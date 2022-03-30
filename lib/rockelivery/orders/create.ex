d83529549297493e483aedf96db8650994215e3b
alexandra <alexandrabsouz@gmail.com>
2022-03-30 12:07:22 -0300

    style: mix format

--- lib/rockelivery/orders/create.ex
+++ lib/rockelivery/orders/create.ex
@@ -15,6 +15,8 @@ defmodule Rockelivery.Orders.Create do
     |> handle_items(params)
   end
 
+  defp handle_items({:error, result}, _params), do: {:error, Error.build(:bad_request, result)}
+
   defp handle_items({:ok, items}, params) do
     params
     |> Order.changeset(items)
@@ -25,8 +27,6 @@ defmodule Rockelivery.Orders.Create do
   defp handle_insert({:ok, %Order{}} = order), do: order
 
   defp handle_insert({:error, result}), do: {:error, Error.build(:bad_request, result)}
-
-  defp handle_items({:error, result}, _params), do: {:error, Error.build(:bad_request, result)}
 
   defp fetch_items(%{"items" => items_params}) do
     items_ids = Enum.map(items_params, fn item -> item["id"] end)
@@ -39,20 +39,20 @@ defmodule Rockelivery.Orders.Create do
   end
 
   defp validate_and_multiply_items(items, items_ids, items_params) do
-    item_map = Map.new(items, fn item -> {item.id, item} end)
+    items_map = Map.new(items, fn item -> {item.id, item} end)
 
     items_ids
-    |> Enum.map(fn id -> {id, Map.get(item_map, id)} end)
+    |> Enum.map(fn id -> {id, Map.get(items_map, id)} end)
     |> Enum.any?(fn {_id, value} -> is_nil(value) end)
-    |> multiply_items(item_map, items_params)
+    |> multiply_items(items_map, items_params)
   end
 
-  defp multiply_items(true, _item_map, _items_params), do: {:error, "inválid items!"}
+  defp multiply_items(true, _items, _items_params), do: {:error, "Invalid ids"}
 
-  defp multiply_items(false, item_map, items_params) do
+  defp multiply_items(false, items, items_params) do
     items =
       Enum.reduce(items_params, [], fn %{"id" => id, "quantity" => quantity}, acc ->
-        item = Map.get(item_map, id)
+        item = Map.get(items, id)
         acc ++ List.duplicate(item, quantity)
       end)
 
