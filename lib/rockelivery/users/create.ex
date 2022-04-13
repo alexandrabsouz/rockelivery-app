c7f0c96d5876165d1f0b7ed9beaff5ae15cd3132
alexandra <alexandrabsouz@gmail.com>
2022-04-13 12:28:55 -0300

    style: mix format

--- lib/rockelivery/users/create.ex
+++ lib/rockelivery/users/create.ex
@@ -8,16 +8,15 @@ defmodule Rockelivery.Users.Create do
   alias ViaCep.Client
 
   def call(%{"cep" => cep} = params) do
-
     changeset = User.changeset(params)
 
     with {:ok, %User{}} <- User.build(changeset),
          {:ok, _cep_info} <- Client.get_cep_info(cep),
          {:ok, %User{}} = user <- Repo.insert(changeset) do
-        user
-         else
-          {:error, %Error{}} = error -> error
-          {:error, result} -> {:error, Error.build(:bad_request, result)}
+      user
+    else
+      {:error, %Error{}} = error -> error
+      {:error, result} -> {:error, Error.build(:bad_request, result)}
     end
   end
 end
