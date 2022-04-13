0082afdd200222b7ef6d22160aae188634f37159
alexandra <alexandrabsouz@gmail.com>
2022-04-13 12:28:25 -0300

    refactor: improving performance and removing unnecessary functions

--- lib/rockelivery/users/create.ex
+++ lib/rockelivery/users/create.ex
@@ -8,25 +8,16 @@ defmodule Rockelivery.Users.Create do
   alias ViaCep.Client
 
   def call(%{"cep" => cep} = params) do
-    with {:ok, %User{} = user} <- User.build(params),
-         {:ok, _cep_info} <- Client.get_cep_info(cep) do
-        Repo.insert(user)
+
+    changeset = User.changeset(params)
+
+    with {:ok, %User{}} <- User.build(changeset),
+         {:ok, _cep_info} <- Client.get_cep_info(cep),
+         {:ok, %User{}} = user <- Repo.insert(changeset) do
+        user
          else
           {:error, %Error{}} = error -> error
           {:error, result} -> {:error, Error.build(:bad_request, result)}
     end
-  end
-
-  defp create_user(params) do
-    params
-    |> User.changeset()
-    |> Repo.insert()
-    |> handle_result()
-  end
-
-  defp handle_result({:ok, %User{}} = result), do: result
-
-  defp handle_result({:error, result}) do
-    {:error, Error.build(:bad_request, result)}
   end
 end
