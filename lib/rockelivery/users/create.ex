ed87c05055ae395454c61638ed8a29d7f72b2682
alexandra <alexandrabsouz@gmail.com>
2022-04-13 11:57:09 -0300

    feat: with in call create user

--- lib/rockelivery/users/create.ex
+++ lib/rockelivery/users/create.ex
@@ -8,10 +8,13 @@ defmodule Rockelivery.Users.Create do
   alias ViaCep.Client
 
   def call(%{"cep" => cep} = params) do
-    case Client.get_cep_info(cep) do
-      {:ok, _cep_info} -> create_user(params)
-      {:error, _error} = error -> error
-    end 
+    with {:ok, %User{} = user} <- User.build(params),
+         {:ok, _cep_info} <- Client.get_cep_info(cep) do
+        Repo.insert(user)
+         else
+          {:error, %Error{}} = error -> error
+          {:error, result} -> {:error, Error.build(:bad_request, result)}
+    end
   end
 
   defp create_user(params) do
