b13b6a69fdd65cc3e5398b69bd8851bb9d784840
alexandra <alexandrabsouz@gmail.com>
2022-05-07 13:03:33 -0300

    feat: set seeds datas

--- priv/repo/seeds.exs
+++ priv/repo/seeds.exs
@@ -9,3 +9,43 @@
 #
 # We recommend using the bang functions (`insert!`, `update!`
 # and so on) as they will fail if something goes wrong.
+
+alias Rockelivery.{Item, Order, User, Repo}
+
+user = %User{
+    age: 23,
+    address: "Rua Tocantins",
+    cep: "68165000",
+    cpf: "23456787998",
+    password: "teste123456",
+    name: "Alexandra Souza"
+}
+
+%User{id: id} = Repo.insert!(user)
+
+item1 = %Item{
+    category: :food,
+    description: "Batatinha Rugles",
+    price: Decimal.new("15.00"),
+    photo: "priv/photos/batatinha.jpg"
+}
+
+item2 = %Item{
+    category: :food,
+    description: "Bolacha Recheada",
+    price: Decimal.new("4.00"),
+    photo: "priv/photos/bolacha.jpg"
+}
+
+Repo.insert!(item1)
+Repo.insert!(item2)
+
+order = %Order{
+    user_id: id,
+    items: [item1, item1, item2],
+    address: "Rua tocantins",
+    comments: "Bolachinha da Oreo e Batata sabor Cebola",
+    payment_method: :money
+}
+
+Repo.insert!(order)
